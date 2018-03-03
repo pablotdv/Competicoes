157d68db4ba415ecc0f13bf1f0e64c323532e4b9
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-03-03 14:48:28 -0300

    Add project files.

---
+++ Competicoes.UnitTests/Models/PaisTests.cs
@@ -0,0 +1,96 @@
+﻿using Competicoes.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Competicoes.UnitTests.Models
+{
+    public class PaisTests
+    {
+        [Fact]
+        public void Pais_Nome_Obrigatorio()
+        {
+            var pais = new Pais()
+            {
+                PaisId = Guid.NewGuid(),
+                Nome = "",
+                Sigla = "BR",
+            };
+
+            var result = ValidateModel(pais);
+
+            Assert.True(result.Count == 1);
+            Assert.Equal("O campo País é obrigatório", result.First().ErrorMessage);
+        }
+
+        [Fact]
+        public void Pais_Nome_Maximo()
+        {
+            var pais = new Pais()
+            {
+                PaisId = Guid.NewGuid(),
+                Nome = new string('*', 201),
+                Sigla = "BR",
+            };
+
+            var result = ValidateModel(pais);
+            Assert.True(result.Count == 1);
+            Assert.Equal("O campo País deve ser uma string com um comprimento máximo de 200", result.First().ErrorMessage);
+
+        }
+
+        [Fact]
+        public void Pais_Sigla_Obrigatorio()
+        {
+            var pais = new Pais()
+            {
+                PaisId = Guid.NewGuid(),
+                Nome = "Brasil",
+                Sigla = "",
+            };
+
+            var result = ValidateModel(pais);
+
+            Assert.True(result.Count == 1);
+            Assert.Equal("O campo Sigla é obrigatório", result.First().ErrorMessage);
+        }
+
+        [Fact]
+        public void Pais_Sigla_Maximo()
+        {
+            var pais = new Pais() {
+                PaisId = Guid.NewGuid(),
+                Nome = "Brasil",
+                Sigla = new string('*', 4),
+            };
+
+            var result = ValidateModel(pais);
+
+            Assert.True(result.Count == 1);
+            Assert.Equal("O campo Sigla deve ser uma string com um comprimento máximo de 3", result.First().ErrorMessage);
+        }
+
+        [Fact]
+        public void Pais_Id_Vazio()
+        {
+            var pais = new Pais()
+            {
+                Nome = "Brasil",
+                Sigla = "BR"
+            };
+
+            Assert.Equal(Guid.Empty, pais.PaisId);
+        }
+
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
+    }
+}
---
+++ Competicoes/Models/Pais.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Competicoes.Models
+{
+    public class Pais
+    {
+        public Guid PaisId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "País")]
+        [StringLength(200, ErrorMessage = "O campo {0} deve ser uma string com um comprimento máximo de {1}")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(3, ErrorMessage = "O campo {0} deve ser uma string com um comprimento máximo de {1}")]
+        public string Sigla { get; set; }
+    }
+}
